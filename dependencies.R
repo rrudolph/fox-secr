a8f7f56b44ac4b22f7177492c56ebcf2d0738c13
Rocky Rudolph <rockne@gmail.com>
2019-08-09 10:05:44 -0700

    modified dependencies code

--- dependencies.R
+++ dependencies.R
@@ -1,17 +1,22 @@
-packages <- c(
-"glue",
-"tidyverse",
-"janitor",
-"xlsx",
-"here",
-"sp",
-"mapview",
-"leaflet",
-"sf",
-"rgdal",
-"rgeos",
-"secr",
-"tools"
+pkgList <- c(
+  "glue",
+  "tidyverse",
+  "janitor",
+  "xlsx",
+  "here",
+  "sp",
+  "mapview",
+  "leaflet",
+  "sf",
+  "rgdal",
+  "rgeos",
+  "secr",
+  "tools"
 )
 
-install.packages(packages)
+
+inst <- pkgList %in% installed.packages()
+if (length(pkgList[!inst]) > 0) install.packages(pkgList[!inst],
+                                                 repos='http://cran.cnr.berkeley.edu/')
+lapply(pkgList, library, character.only = TRUE)
+
