9145c2afd1c1bdd8c2d0f1515e46cca37c567132
Rocky Rudolph <rockne@gmail.com>
2021-04-01 17:35:01 -0700

    explained dependencies script with comments.

--- dependencies.R
+++ dependencies.R
@@ -1,3 +1,7 @@
+# If upgrading to a new version of R, or starting out for the first time,
+# run this to get all packages needed. 
+
+
 pkgList <- c(
   "glue",
   "tidyverse",
@@ -14,8 +18,10 @@ pkgList <- c(
   "tools"
 )
 
-
+# Install the packages if they are not already.
 inst <- pkgList %in% installed.packages()
-if (length(pkgList[!inst]) > 0) install.packages(pkgList[!inst])
+if (length(pkgList[!inst]) > 0) install.packages(pkgList[!inst]))
+
+# Optional to load them, but typically just load them in the scripts.
 # lapply(pkgList, library, character.only = TRUE)
 
